6d898956cba97b61aabc637a491df4e2fb7204a4
kfilyk <kelvinfilyk@gmail.com>
2021-11-15 13:49:54 -0800

    Added link to old, broken vs of petri

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = (props) => {
         <a href="https://github.com/kfilyk/cpp_accelerated_image_quantization">K-MEANS SEGMENTATION (C++)</a>
         <a href="https://certn-dash.herokuapp.com/">CERTN DASHBOARD</a>
         <a href="https://carechanger.herokuapp.com/">CARECHANGER</a>
-        <a>PETRI</a>
+        <a href="https://kfilyk.github.io/petri/">PETRI</a>
         <a>QUORACLE</a>
         <a>MORI DUNOM</a>
       </div>
