a4dd28d4e6d0206125cbceb219064ef129cf6707
kfilyk <kelvinfilyk@gmail.com>
2021-11-15 10:38:54 -0800

    Attempting to add redirection from empty start page to /portfolio

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
     index: "./src/index.tsx",
   },
   output: {
-    path: resolve(__dirname, "dist"),
+    path: resolve(__dirname, "docs"),
     filename: "bundle.js",
   },
   resolve: {
